615afebdba20893ce964c92b261da0b0728ff85c
wesleyeewong <wesleywong1993@gmail.com>
2015-10-17 19:45:40 -0600

    hello world /addVideo

--- addVideo/views.py
+++ addVideo/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+def index(request):
+	template = loader.get_template('addVideo/index.html')
+	context = RequestContext(request, {
+		'argument': 'hello world',
+		})
+	return HttpResponse(template.render(context))
