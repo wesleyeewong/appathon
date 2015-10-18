b5409b59a88dc142725b1275e641a01f3a284e0f
Kenny Shu <kshu6@gatech.edu>
2015-10-18 07:22:02 -0400

    v

--- addVideo/views.py
+++ addVideo/views.py
@@ -3,10 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from youtube_search import youtube_search
 
 def index(request):
 	template = loader.get_template('addVideo/index.html')
-	context = RequestContext(request, {
-		'nameList': ["wesley","kenny","kristy"],
-		})
-	return HttpResponse(template.render(context))
+	context = youtube_search("perry")
+	return HttpResponse(template.render({'context':context}))
