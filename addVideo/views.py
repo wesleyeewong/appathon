97a96cce5798273d25eadeb9af34f9fd5206a308
wesleyeewong <wesleywong1993@gmail.com>
2015-10-18 00:58:18 -0600

    bleh

--- addVideo/views.py
+++ addVideo/views.py
@@ -3,10 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+import create_playlist
 
 def index(request):
 	template = loader.get_template('addVideo/index.html')
 	context = RequestContext(request, {
 		'nameList': ["wesley","kenny","kristy"],
 		})
+
 	return HttpResponse(template.render(context))
