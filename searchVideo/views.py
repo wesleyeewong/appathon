827b0bb9da144f81d98d763c66fd2a7ab1e6838d
Kenny Shu <kshu6@gatech.edu>
2015-10-18 06:19:46 -0400

    d

--- searchVideo/views.py
+++ searchVideo/views.py
@@ -6,10 +6,12 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from show_playlist_songs import show_playlist_songs
 
 def index(request):
-	template = loader.get_template('addVideo/index.html')
+	template = loader.get_template('searchVideo/index.html')
 	context = RequestContext(request, {
-		'nameList': ["wesley","kenny","kristy"],
+		'nameList': ["kenny","kenny","kenny"],
 		})
-	return HttpResponse(template.render(context))
+	context = show_playlist_songs("PLwDI47x9eHsG9Bqj1L0JZaKLyKcssviDj")
+	return HttpResponse(template.render({'context':context}))
