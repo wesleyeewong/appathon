7db54731f32df789b3fbfd81dd4729ae2bff1204
Kenny Shu <kshu6@gatech.edu>
2015-10-18 04:45:11 -0400

    paper

--- searchVideo/views.py
+++ searchVideo/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+def index(request):
+	template = loader.get_template('addVideo/index.html')
+	context = RequestContext(request, {
+		'nameList': ["wesley","kenny","kristy"],
+		})
+	return HttpResponse(template.render(context))
