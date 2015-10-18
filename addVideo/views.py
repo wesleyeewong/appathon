5ed1f40dc2d4cbb1b38f11adbe0514ae25befd9b
wesleyeewong <wesleywong1993@gmail.com>
2015-10-18 06:25:15 -0600

    working appgit add --all!

--- addVideo/views.py
+++ addVideo/views.py
@@ -4,8 +4,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from youtube_search import youtube_search
+from forms import NameForm
 
 def index(request):
+	if request.method == 'POST':
+		form = NameForm(request.POST)
+	search = request.POST['search']
+	id = request.POST.get('playlistID', 'PLwDI47x9eHsFO3r9TxGovXpWbLwDhKcSC')
 	template = loader.get_template('addVideo/index.html')
-	context = youtube_search("perry")
-	return HttpResponse(template.render({'context':context}))
+	context = youtube_search(search)
+	return HttpResponse(template.render({'context':context, 'id':id}))
